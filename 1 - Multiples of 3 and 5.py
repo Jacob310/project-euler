ba28a68534a87935465ad4e89a534c8f8868bc7e
jellis_helyx <j.ellis@helyx.co.uk>
2020-12-28 19:37:29 +0000

    remove repeated if statement

--- 1 - Multiples of 3 and 5.py
+++ 1 - Multiples of 3 and 5.py
@@ -8,9 +8,7 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 y=[]
 for i in range(1,1000):
     x = ""
-    if i % 3 == 0:
-        x = i
-    if i % 5 == 0:
+    if i % 3 == 0 or i % 5 == 0:
         x = i
     if x != "":
         #print(x)
